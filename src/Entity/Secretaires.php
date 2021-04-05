25219f6143f9b50d7caf50d57ac9ea3cecad4f9f
VucicA <antoine.vucic@gmail.com>
2021-04-05 16:49:04 +0200

    DB secretaires

--- src/Entity/Secretaires.php
+++ src/Entity/Secretaires.php
@@ -23,6 +23,26 @@ class Secretaires
      */
     private $user_id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomSecretaire;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenomSecretaire;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mailSecretaire;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -36,6 +56,54 @@ class Secretaires
     public function setUserId(users $user_id): self
     {
         $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getNomSecretaire(): ?string
+    {
+        return $this->nomSecretaire;
+    }
+
+    public function setNomSecretaire(string $nomSecretaire): self
+    {
+        $this->nomSecretaire = $nomSecretaire;
+
+        return $this;
+    }
+
+    public function getPrenomSecretaire(): ?string
+    {
+        return $this->prenomSecretaire;
+    }
+
+    public function setPrenomSecretaire(string $prenomSecretaire): self
+    {
+        $this->prenomSecretaire = $prenomSecretaire;
+
+        return $this;
+    }
+
+    public function getMailSecretaire(): ?string
+    {
+        return $this->mailSecretaire;
+    }
+
+    public function setMailSecretaire(string $mailSecretaire): self
+    {
+        $this->mailSecretaire = $mailSecretaire;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
 
         return $this;
     }
